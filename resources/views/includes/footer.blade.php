97997236c0ff0ecb1979d08f6ff919941a4fa02d
Scipo <ivots@mail.bg>
2016-11-11 14:40:01 +0200

    Footer section of the web page is added

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<body>
+<hr>
+<footer>
+    <div class="footer-side">
+        <ul>
+            <li>
+                <a href="{{ URL::to('/program')}}">Програма</a>
+            </li>
+            <li>
+                <a href="{{ URL::to('/terms')}}">Регламент</a>
+            </li>
+            <li>
+                <a href="{{ URL::to('/apply-for-attendance')}}">Форма за участие</a>
+            </li>
+            <li>
+                <a href="{{ URL::to('/about')}}">За нас</a>
+            </li>
+            <li>
+                <a href="{{ URL::to('/vision')}}">Визия</a>
+            </li>
+        </ul>
+    </div>
+    <div class="footer-side">
+        <ul>
+            <li>
+                <a href="#">Стипендии за бакалаври</a>
+            </li>
+            <li>
+                <a href="#">Стипендии за магистри</a>
+            </li>
+            <li>
+                <a href="#">Стипендии за докторанти</a>
+            </li>
+            <li>
+                <a href="{{ URL::to('/authors')}}">Автори</a>
+            </li>
+        </ul>
+    </div>
+    <div class="footer-side">
+        <ul>
+            <li>
+                <a href="#">Изложби</a>
+            </li>
+            <li>
+                <a href="#">Ателиета</a>
+            </li>
+            <li>
+                <a href="#">Лекции</a>
+            </li>
+            <li>
+                <a href="#">Галерия</a>
+            </li>
+        </ul>
+    </div>
+    <div class="footer-side">
+        <ul>
+            <li>
+                <a href="{{ URL::to('/about')}}">За нас</a>
+            </li>
+            <li>
+                <a href="{{ URL::to('/partners')}}">Партньори</a>
+            </li>
+            <li>
+                <a href="{{ URL::to('/contact')}}">Контакти</a>
+            <li>
+                <a href="{{ URL::to('/archive')}}">Архиви</a>
+            </li>
+        </ul>
+    </div>
+    <div class="footer-email-send">
+        <form class="footer-info">
+            <label for="email">Месечен бюлетин</label><br>
+            <input type="email" name="email" placeholder="e-mail"><br><br>
+            <input type="submit" value="Запиши се">
+        </form>
+
+    </div>
+</footer>
+</body>
+</html>
