05dce5a7a6eeec8822c8f3a9b3fb9b875985da3e
Petar Ivanov <petar.ivanov256@gmail.com>
2016-11-17 19:56:12 +0200

    -Merged conflict resolved

--- resources/views/applyForAttendanceForm.blade.php
+++ resources/views/applyForAttendanceForm.blade.php
@@ -1,14 +1,15 @@
+@extends('pages.ApplyForAttendanceForm')
+@section('content')
 
-
-{!! Form::open(['action' => 'FormsController@applyForAttendanceForm', 'method' => 'post']) !!}
+    {!! Form::open(['action' => 'FormsController@applyForAttendanceForm', 'method' => 'post']) !!}
     {!!Form::label('test', 'Test')!!}
     {!!Form::text('test', 'Text here')!!}
     {!!Form::token()!!}
     {!! Form::submit('Send text!', array('class' => 'btn btn-primary'))!!}
-{!! Form::close() !!}
+    {!! Form::close() !!}
 
 
     @if(!empty($data))
-       <h1>{{$data}}</h1>
+        <h1>{{$data}}</h1>
     @endif
-
+@stop
