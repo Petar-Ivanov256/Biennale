fea49d9a5dcf202f6ce8646b61a5f2cceeaec2a5
Scipo <ivots@mail.bg>
2016-12-02 12:09:38 +0200

    bootstrap little change

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,6 +3,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="{{ url('') }}./css/bootstrap.min.css" type="text/css"/>
+
 <link rel="stylesheet" href="{{ url('') }}./css/app.css" type="text/css"/>
 <link rel="stylesheet" href="{{url('')}}./fontawesome/css/font-awesome.min.css">
