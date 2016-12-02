1df6f2ac8f91d24a1d9f7394f7fa7fae62c89ec1
Scipo <ivots@mail.bg>
2016-12-02 12:25:54 +0200

    little change of contact.blade

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,6 +11,7 @@
         <p id="SuzIstoria">Създаваме история</p>
         <p id="purvoMezdunarodno">Първото международно биенале в България</p>
     </div>
+
     <div id="contentInfo" class="FirstTable">
         <h1 id="Title1">Контакти</h1><br>
         <div>
@@ -20,6 +21,30 @@
                 ул. Номередно 12<br>
             </p>
         </div>
+        <div class="table-responsive">
+            <table class="table">
+                <tbody>
+                <tr>
+                    <td>Александра Велева</td>
+                    <td>alex.kotseva@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>кординатор</td>
+                    <td>+359 887 288 667</td>
+                <tr>
+                <tr>
+                    <td>София-Хара Хадзопулу</td>
+                    <td>sofia.hadzopoulu@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>кординатор</td>
+                    <td>+359 887 288 667</td>
+                <tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
     <div class="col-md-8 col-md-offset-8" id="contact">
         <h2 id="Title2" class="col-md-offset-4" >Контактна Форма</h2>
         <form method="POST" action="{{ URL::to('/apply') }}" class="form-horizontal"><br><br>
@@ -50,4 +75,5 @@
             </div>
         </form>
     </div>
+
 @endsection
