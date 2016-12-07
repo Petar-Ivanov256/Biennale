4a212f1a17ad8c956fadd0c36998597037a6a94a
Scipo <ivots@mail.bg>
2016-12-08 01:07:17 +0200

    minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
     <div  class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-sm-1">
-                <p id="SuzIstoria">Създаваме<br>история</p>
+                <p id="SuzIstoria">Създаваме история</p>
                 <p id="purvoMezdunarodno">Първото международно<br>биенале за стъкло<br>в България</p>
             </div>
         </div>
