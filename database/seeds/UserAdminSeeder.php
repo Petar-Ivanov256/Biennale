f4397e8f432996227ce14fd31a47c830572c0998
Petar Ivanov <petar.ivanov256@gmail.com>
2016-12-11 15:47:18 +0200

    Add fake user

---
+++ database/seeds/UserAdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'Gosho';
+        $user->email = 'gosho@admin.com';
+        $user->password = bcrypt('123456');
+        $user->role_id = Role::where('title', 'admin')->first()->id;
+        $user->save();
+    }
+}
