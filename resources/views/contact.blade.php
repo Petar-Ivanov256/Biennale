931285e512b52f12d4b13ba2e97de240f9733b29
Scipo <ivots@mail.bg>
2016-12-22 23:06:27 +0200

    contact.blade.php is a partially repaired

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,8 +4,7 @@
 @endsection
 
 @section('content')
-    <section>
-        <div class="container">
+         <div class="container">
             <div class="row">
                 <div class="col-md-3 SideDate">
                     <p>27.09-02.10</p>
@@ -21,57 +20,63 @@
             </div>
         </div>
 
+        <div  class="container">
+              <div class="col-lg-12">
+                    <div class="col-lg-6">
+                        <h1 id="Title1">Контакти</h1><br>
+                        <p id="contactText">
+                            Международно биенале на стъклото<br>
+                            София 1000, България<br>
+                            ул. Номередно №12<br>
+                        </p>
+                        <p>
+                            <a href="mailto:glassbiennalebg@gmail.com" id="emailCordinator">glassbiennalebg@gmail.com</a>
+                        </p>
+                        <div class="divspancontact">
+                            <span id="nameForContact">Александра Велева</span>
+                            <span><a href="mailto:alex.kotseva@gmail.com" id="emailCordinator">alex.kotseva@gmail.com</a></span>
+                            <span id="cordinator">координатор</span>
+                            <span><a href="tel:+359887288667" class="telefonCordinator">+359 887 288 667</a></span>
+                            <span id="nameForContact">София-Хара Хадзопулу</span>
+                            <span><a href="mailto:sofia.hadzopoulu@gmail.com" id="emailCordinator">sofia.hadzopoulu@gmail.com</a></span>
+                            <span id="cordinator">координатор</span>
+                            <span><a href="tel:+359887288667" class="telefonCordinator">+359 887 288 667</a></span>
+                       </div>
+                    </div>
+                </div>
+            </div>
+       </div>
 
-        <div id="contentInfo" class="container-fluid">
-        <h1 id="Title1">Контакти</h1><br>
-        <div id="contactText">
-            <p>
-                Международно биенале на стъклото<br>
-                София 1000, България<br>
-                ул. Номередно 12<br>
-            </p>
-            <p>
-                <a id="emailCordinator">glassbiennalebg@gmail.com</a>
-            </p>
-            <span id="nameForContact">Александра Велева</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span><a id="emailCordinator">alex.kotseva@gmail.com</a></span>
-            <span id="cordinator">координатор</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span id="telefonCordinator">+359 887 288 667</span><br>
-            <span id="nameForContact">София-Хара Хадзопулу</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span><a id="emailCordinator">sofia.hadzopoulu@gmail.com</a></span><br>
-            <span id="cordinator">координатор</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span id="telefonCordinator">+359 887 288 667</span><br>
-        </div>
-    </div>
-
-    <div class="col-md-6 col-md-offset-6 container-fluid" id="contact">
-        <h2 id="Title2" class="col-md-offset-4" >Контактна Форма</h2>
-        <form method="POST" action="{{ URL::to('/apply') }}" class="form-horizontal"><br><br>
-            <div class="form-group" id="contentContactForm">
-                <div class="col-md-4 col-md-offset-4">
-                    <input type="text" class="form-control" id="name" name="name" placeholder="Име и фамилия*" value="{{ Request::old('name') }}">
-                </div>
-            </div>
-            <div class="form-group" id="contentContactForm">
-                <div class="col-md-4 col-md-offset-4">
-                    <input type="text" class="form-control" id="email" name="email" placeholder="E-mail*" value="{{ Request::old('email') }}">
-                </div>
-            </div>
-            <div class="form-group" id="contentContactForm">
-                <div class="col-md-4 col-md-offset-4">
-                    <input type="text" class="form-control" id="title" name="Title" placeholder="Заглавие*" value="{{ Request::old('englishTitle') }}">
-                </div>
-            </div>
-            <div class="form-group" id="contentContactForm">
-                <div class="col-md-4 col-md-offset-4">
-                    <textarea class="form-control" id="Message" name="Message" cols="25" rows="5" placeholder="Съобщение*"></textarea>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-lg-4 col-lg-offset-7" id="submitButton">
-                    <button type="submit" class="contactButon">Изпрати&nbsp;<i class="fa fa-chevron-circle-right" id="fa-fa-chevron-right"></i></button>
-                </div>
-            </div>
-        </form>
-    </div>
-</section>
-
-    <!-- TODO Validations for contact Form--->
+         <div class="col-md-6 col-md-offset-6 container-fluid" id="contact">
+             <h2 id="Title2" class="col-md-offset-4" >Контактна Форма</h2>
+             <form method="POST" action="{{ URL::to('/apply') }}" class="form-horizontal"><br><br>
+                 <div class="form-group" id="contentContactForm">
+                     <div class="col-md-4 col-md-offset-4">
+                         <input type="text" class="form-control" id="name" name="name" placeholder="Име и фамилия*" value="{{ Request::old('name') }}">
+                     </div>
+                 </div>
+                 <div class="form-group" id="contentContactForm">
+                     <div class="col-md-4 col-md-offset-4">
+                         <input type="text" class="form-control" id="email" name="email" placeholder="E-mail*" value="{{ Request::old('email') }}">
+                     </div>
+                 </div>
+                 <div class="form-group" id="contentContactForm">
+                     <div class="col-md-4 col-md-offset-4">
+                         <input type="text" class="form-control" id="title" name="Title" placeholder="Заглавие*" value="{{ Request::old('englishTitle') }}">
+                     </div>
+                 </div>
+                 <div class="form-group" id="contentContactForm">
+                     <div class="col-md-4 col-md-offset-4">
+                         <textarea class="form-control" id="Message" name="Message" cols="25" rows="5" placeholder="Съобщение*"></textarea>
+                     </div>
+                 </div>
+                 <div class="form-group">
+                     <div class="col-lg-4 col-lg-offset-7" id="submitButton">
+                         <button type="submit" class="contactButon">Изпрати&nbsp;<i class="fa fa-chevron-circle-right" id="fa-fa-chevron-right"></i></button>
+                     </div>
+                 </div>
+             </form>
+         </div>
+         <!-- TODO Validations for contact Form--->
 
 @endsection
