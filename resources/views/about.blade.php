5154a6e96759f7c2947e2c64eabd56863766e2a8
Scipo <ivots@mail.bg>
2016-12-23 15:54:31 +0200

    about.blade.php is fully repaired

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -17,131 +17,133 @@
         </div>
 
     <div class="container">
+        <div class="col-lg-12">
+             <div class="col-xs-12 col-sm-6 col-lg-5 aboutleft">
+                    <h1 class="name1">Нашата визия</h1>
+                    <p class="textAbout">
+                        Международно биенале на стъклото е първото по рода си събитие, което се състои края на месец септември на всеки две години в България.
+                        Събитията на биеналето се провеждат в три града - столицата София, морският град Варна и град Белослав, известен с фабриката си за стъкло.
+                        Нашата визия е общество, за което изкуството е неразделна част  от живота и света, за да спомогне за осъществяването на пъстроцветно бъдеще за идните поколения.
+                        Работейки в тази посока, ние вдъхновяваме авторите по света, творящи в областта на художественото стъкло, да представят и споделят своите знания.
+                        Целта ни е развитие на изкуството от стъкло в Европа и по-специално в източната част на континента. Ние припознаваме определени ценности като екзистенциални за достигането на нашите мисия и визия.
+                        Като екип от артисти самите ние сме мотивирани от следните ценности: уважение, сътрудничество, качество, новаторство, обогатяване на познанията в областта, вдъхновение и позитивно мислене.
+                    </p>
+                    <h2 class="name1">Екип</h2>
+                    <div>
+                        <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#myModal">
+                            Арт Директор
+                        </button><br>
+                        <!-- Modal -->
+                        <div class="modal fade bs-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="myModalLabel"> Арт Директор</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#mycoordinatorModal">
+                            Координатори
+                        </button><br>
+                        <!-- Modal -->
+                        <div class="modal fade bs-example-modal-lg" id="mycoordinatorModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="myModalLabel"> Координатори</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#myJuryModal">
+                            Жури
+                        </button><br>
+                        <!-- Modal -->
+                        <div class="modal fade bs-example-modal-lg" id="myJuryModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="myModalLabel"> Жури</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 
-            <div class="col-xs-12 col-sm-6 col-lg-12">
-               <div class="col-xs-12 col-sm-6 col-lg-6">
-                <h1 class="name1">Нашата визия</h1>
-                <p class="textAbout">
-                    Международно биенале на стъклото е първото по рода си събитие, което се състои края на месец септември на всеки две години в България.
-                    Събитията на биеналето се провеждат в три града - столицата София, морският град Варна и град Белослав, известен с фабриката си за стъкло.
-                    Нашата визия е общество, за което изкуството е неразделна част  от живота и света, за да спомогне за осъществяването на пъстроцветно бъдеще за идните поколения.
-                    Работейки в тази посока, ние вдъхновяваме авторите по света, творящи в областта на художественото стъкло, да представят и споделят своите знания.
-                    Целта ни е развитие на изкуството от стъкло в Европа и по-специално в източната част на континента. Ние припознаваме определени ценности като екзистенциални за достигането на нашите мисия и визия.
-                    Като екип от артисти самите ние сме мотивирани от следните ценности: уважение, сътрудничество, качество, новаторство, обогатяване на познанията в областта, вдъхновение и позитивно мислене.
-                </p>
-            </div>
-                <div class="picAbout">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#myDesignModal">
+                            Графичен дизаин
+                        </button><br>
+                        <!-- Modal -->
+                        <div class="modal fade bs-example-modal-lg" id="myDesignModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="myModalLabel">Графичен дизаин</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#myWebModal">
+                            Уеб дизаин
+                        </button><br>
+                        <!-- Modal -->
+                        <div class="modal fade bs-example-modal-lg" id="myWebModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="myModalLabel">Уеб дизаин</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <section>
+                <div class="col-lg-5 picAbout">
                     {!! HTML::image('img/picturesFromAbout.jpg') !!}
                 </div>
-        <div class="col-xs-12 col-sm-6 col-md-2">
-                <h2 class="name2">Екип</h2>
-                <div>
-                    <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#myModal">
-                        Арт Директор
-                    </button><br>
-                    <!-- Modal -->
-                    <div class="modal fade bs-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <h4 class="modal-title" id="myModalLabel"> Арт Директор</h4>
-                                </div>
-                                <div class="modal-body">
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#mycoordinatorModal">
-                        Координатори
-                    </button><br>
-                    <!-- Modal -->
-                    <div class="modal fade bs-example-modal-lg" id="mycoordinatorModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <h4 class="modal-title" id="myModalLabel"> Координатори</h4>
-                                </div>
-                                <div class="modal-body">
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#myJuryModal">
-                        Жури
-                    </button><br>
-                    <!-- Modal -->
-                    <div class="modal fade bs-example-modal-lg" id="myJuryModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <h4 class="modal-title" id="myModalLabel"> Жури</h4>
-                                </div>
-                                <div class="modal-body">
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </section>
+              </div>
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#myDesignModal">
-                        Графичен дизаин
-                    </button><br>
-                    <!-- Modal -->
-                    <div class="modal fade bs-example-modal-lg" id="myDesignModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <h4 class="modal-title" id="myModalLabel">Графичен дизаин</h4>
-                                </div>
-                                <div class="modal-body">
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="button" class="aboutButtons btn-lg" data-toggle="modal" data-target="#myWebModal">
-                        Уеб дизаин
-                    </button><br>
-                    <!-- Modal -->
-                    <div class="modal fade bs-example-modal-lg" id="myWebModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <h4 class="modal-title" id="myModalLabel">Уеб дизаин</h4>
-                                </div>
-                                <div class="modal-body">
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
            </div>
-         </div>
-      </div>
+
+        </div>
 
 @endsection
 
