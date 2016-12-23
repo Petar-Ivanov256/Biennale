f4381cc1934c9807fa4a9183f403f4dad6191ed0
Scipo <ivots@mail.bg>
2016-12-23 13:04:31 +0200

    contact.blade.php is fully repaired

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -21,9 +21,10 @@
         </div>
 
         <div  class="container">
-              <div class="col-lg-12">
-                    <div class="col-lg-6">
-                        <h1 id="Title1">Контакти</h1><br>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="col-lg-4">
+                        <h1 class="Title1">Контакти</h1><br>
                         <p id="contactText">
                             Международно биенале на стъклото<br>
                             София 1000, България<br>
@@ -34,49 +35,50 @@
                         </p>
                         <div class="divspancontact">
                             <span id="nameForContact">Александра Велева</span>
-                            <span><a href="mailto:alex.kotseva@gmail.com" id="emailCordinator">alex.kotseva@gmail.com</a></span>
+                            <span class="contactrightspan"><a href="mailto:alex.kotseva@gmail.com" id="emailCordinator">alex.kotseva@gmail.com</a></span>
                             <span id="cordinator">координатор</span>
-                            <span><a href="tel:+359887288667" class="telefonCordinator">+359 887 288 667</a></span>
+                            <span class="contactrightspan"><a href="tel:+359887288667" class="telefonCordinator">+359 887 288 667</a></span>
                             <span id="nameForContact">София-Хара Хадзопулу</span>
-                            <span><a href="mailto:sofia.hadzopoulu@gmail.com" id="emailCordinator">sofia.hadzopoulu@gmail.com</a></span>
+                            <span class="contactrightspan"><a href="mailto:sofia.hadzopoulu@gmail.com" id="emailCordinator">sofia.hadzopoulu@gmail.com</a></span>
                             <span id="cordinator">координатор</span>
-                            <span><a href="tel:+359887288667" class="telefonCordinator">+359 887 288 667</a></span>
-                       </div>
+                            <span class="contactrightspan"><a href="tel:+359887288667" class="telefonCordinator">+359 887 288 667</a></span>
+                        </div>
+                    </div>
+                    <div class="col-lg-8" id="contact">
+                        <h2 class="Title1" >Контактна Форма</h2>
+                        <form method="POST" action="{{ URL::to('/apply') }}" class="form-horizontal">
+                            <div class="form-group" id="contentContactForm">
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Име и фамилия*" value="{{ Request::old('name') }}">
+                                </div>
+                            </div>
+                            <div class="form-group" id="contentContactForm">
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" id="email" name="email" placeholder="E-mail*" value="{{ Request::old('email') }}">
+                                </div>
+                            </div>
+                            <div class="form-group" id="contentContactForm">
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" id="title" name="Title" placeholder="Заглавие*" value="{{ Request::old('englishTitle') }}">
+                                </div>
+                            </div>
+                            <div class="form-group" id="contentContactForm">
+                                <div class="col-md-5">
+                                    <textarea class="form-control" id="Message" name="Message" cols="25" rows="5" placeholder="Съобщение*"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-5" id="submitButton">
+                                    <button type="submit" class="contactButon">Изпрати&nbsp;<i class="fa fa-chevron-circle-right" id="fa-fa-chevron-right"></i></button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-       </div>
+            <br><br><br>
+        </div>
 
-         <div class="col-md-6 col-md-offset-6 container-fluid" id="contact">
-             <h2 id="Title2" class="col-md-offset-4" >Контактна Форма</h2>
-             <form method="POST" action="{{ URL::to('/apply') }}" class="form-horizontal"><br><br>
-                 <div class="form-group" id="contentContactForm">
-                     <div class="col-md-4 col-md-offset-4">
-                         <input type="text" class="form-control" id="name" name="name" placeholder="Име и фамилия*" value="{{ Request::old('name') }}">
-                     </div>
-                 </div>
-                 <div class="form-group" id="contentContactForm">
-                     <div class="col-md-4 col-md-offset-4">
-                         <input type="text" class="form-control" id="email" name="email" placeholder="E-mail*" value="{{ Request::old('email') }}">
-                     </div>
-                 </div>
-                 <div class="form-group" id="contentContactForm">
-                     <div class="col-md-4 col-md-offset-4">
-                         <input type="text" class="form-control" id="title" name="Title" placeholder="Заглавие*" value="{{ Request::old('englishTitle') }}">
-                     </div>
-                 </div>
-                 <div class="form-group" id="contentContactForm">
-                     <div class="col-md-4 col-md-offset-4">
-                         <textarea class="form-control" id="Message" name="Message" cols="25" rows="5" placeholder="Съобщение*"></textarea>
-                     </div>
-                 </div>
-                 <div class="form-group">
-                     <div class="col-lg-4 col-lg-offset-7" id="submitButton">
-                         <button type="submit" class="contactButon">Изпрати&nbsp;<i class="fa fa-chevron-circle-right" id="fa-fa-chevron-right"></i></button>
-                     </div>
-                 </div>
-             </form>
-         </div>
-         <!-- TODO Validations for contact Form--->
+        <!-- TODO Validations for contact Form--->
 
 @endsection
