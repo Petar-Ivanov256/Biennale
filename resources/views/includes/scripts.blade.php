f714547c81de54ab5209a02536c8715e34e38347
Scipo <ivots@mail.bg>
2016-12-27 15:21:29 +0200

    Bachelor.blade.php is under construction only CSS remains to be done.

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,2 +1,3 @@
 <script src="{{ URL::asset('js/jquery.min.js') }}"></script>
 <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('js/bachelorbuttons.js') }}"></script>
