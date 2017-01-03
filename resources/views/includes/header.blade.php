457ee855f2735c9dcd0bb7e2e7c34ce8e766165c
Scipo <ivots@mail.bg>
2017-01-03 17:44:18 +0200

    Navigation bar is responsive but is not good

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar" role="navigation" >
+<!--<nav class="navbar navbar" role="navigation" >
     <div class="container">
         <div class="logo col-md-2">&nbsp;</div>
         <div class="navbar-header col-md-10">
@@ -70,6 +70,86 @@
                         <li><a href={{url('https://www.pinterest.com/')}}><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
                 </ul>
             </div>
+        </div>
+    </div>
+</nav>-->
+
+<nav class="navbar-default" >
+        <div class="container" id="myNavbar">
+         <div class="navbar-header col-md-11">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#targetNav">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+           <!--  <div class="logo col-sm-2">&nbsp;</div>-->
+        </div>
+        <div class="collapse navbar-collapse col-md-11 navB-left" id="targetNav">
+            <ul class="nav navbar-nav" id="navBar" >
+                <li class="dropdown"><a  class="Bien" href="{{ URL::to('/')}}">Бианале</a>
+                    <ul class="dropdown-menu" id="dropdown-content">
+                        <li>
+                            <a class="Program subMenu" href="{{ URL::to('/program')}}">Програма</a>
+                        </li>
+                        <li>
+                            <a class="AttendencyForm subMenu" href="{{ URL::to('/apply-for-attendance')}}">Форма за участие</a>
+                        </li>
+                        <li>
+                            <a class="About subMenu" href="{{ URL::to('/about')}}">За нас</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav" id="navBar">
+                <li>
+                    <a id="mainMenuComponents" class="Schlarships" href="#">Стипендии</a>
+                    <ul class="dropdown-menu" id="dropdown-content">
+                        <li> <a class="subMenu" href="{{ URL::to('/bachelor')}}">Бакалавър</a></li>
+                        <li> <a class="subMenu" href="{{ URL::to('/master')}}">Магистър</a></li>
+                        <li> <a class="subMenu" href="{{ URL::to('/phd')}}">Докторант</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav" id="navBar">
+                <li>
+                    <a id="mainMenuComponents" class="Authors"href="{{ URL::to('/authors')}}">Автори</a>
+                </li>
+                <li>
+                    <a id="mainMenuComponents" class="Events"href="{{ URL::to('/events')}}">Събития</a>
+                </li>
+                <li>
+                    <a id="mainMenuComponents" class="News"href="{{ URL::to('/news')}}">Новини</a>
+                </li>
+                <li>
+                    <a id="mainMenuComponents" class="Partners" href="{{ URL::to('/partners')}}">Партньори</a>
+                </li>
+                <li>
+                    <a id="mainMenuComponents" class="Archives" href="{{ URL::to('/archive')}}">Архиви</a>
+                </li>
+                <li>
+                    <a id="mainMenuComponents" class="Contacts" href="{{ URL::to('/contact')}}">Контакти</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right" id="IconsMenuComponents">
+                @if(Auth::check())
+                    <li><a href="#">{{ Auth::user()->name }}</a></li>
+
+                    @if (Auth::user()->isAdmin())
+                        <li><a href="{{ URL::to('/admin') }}">Админ</a></li>
+                    @endif
+
+                    <li><a href="{{ URL::to('/logout') }}">Изход</a></li>
+                @else
+                    <li><a href="{{ URL::to('/register') }}">Регистрация</a></li>
+                    <li><a href="{{ URL::to('/signin') }}">Вход</a></li>
+                @endif
+
+
+                <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+                <li><a href={{url('https://www.facebook.com/')}}><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href={{url('https://www.instagram.com/')}}><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                <li><a href={{url('https://www.pinterest.com/')}}><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+            </ul>
         </div>
     </div>
 </nav>
