b0473e28326c4fedc6fe65c417fd7bca5bbb63aa
Scipo <ivots@mail.bg>
2017-01-06 12:27:32 +0200

    footer.blade.php is fixed

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,80 +1,83 @@
 <hr>
-<footer id="BladeFooter" class="container">
-    <div class="col-sm-1" id="Shape1">
-        {!! HTML::image('img/logo.png') !!}
-    </div>
-    <div class="col-sm-2" id="Shape1">
-        {!! HTML::image('img/vector.png') !!}
-    </div>
-    <div class="col-sm-2">
-        <ul>
-            <li>
-                <a  href="{{ URL::to('/program')}}">Програма</a>
-            </li>
-            <li>
-                <a href="{{ URL::to('/apply-for-attendance')}}">Форма за участие</a>
-            </li>
-            <li>
-                <a  href="{{ URL::to('/about')}}">За нас</a>
-            </li>
-            <li>
-                <a  href="{{ URL::to('/vision')}}">Визия</a>
-            </li>
-        </ul>
-    </div>
-    <div class="col-md-2">
-        <ul>
-            <li>
-                <a href="#">Стипендии за бакалаври</a>
-            </li>
-            <li>
-                <a href="#">Стипендии за магистри</a>
-            </li>
-            <li>
-                <a href="#">Стипендии за докторанти</a>
-            </li>
-            <li>
-                <a href="{{ URL::to('/authors')}}">Автори</a>
-            </li>
-        </ul>
-    </div>
-    <div class="col-md-1">
-        <ul>
-            <li>
-                <a href="#">Изложби</a>
-            </li>
-            <li>
-                <a href="#">Ателиета</a>
-            </li>
-            <li>
-                <a href="#">Лекции</a>
-            </li>
-            <li>
-                <a href="#">Галерия</a>
-            </li>
-        </ul>
-    </div>
-    <div class="col-md-2">
-        <ul>
-            <li>
-                <a href="{{ URL::to('/about')}}">За нас</a>
-            </li>
-            <li>
-                <a href="{{ URL::to('/partners')}}">Партньори</a>
-            </li>
-            <li>
-                <a href="{{ URL::to('/contact')}}">Контакти</a>
-            <li>
-                <a href="{{ URL::to('/archive')}}">Архиви</a>
-            </li>
-        </ul>
-    </div>
-    <div class="col-md-2">
-        <form>
-            <label for="email" id="Newsletter">Месечен бюлетин</label><br>
-            <input type="email" name="email" placeholder="e-mail" id="e-mail"><br><br>
-            <input type="submit" value="Запиши се">
-        </form>
+<footer id="BladeFooter">
+   <div class="container">
+       <div class="row">
+           <div class="col-md-10 footer-move-left" id="return-footer">
+               <div class="col-md-2">
+                   <p id="footer_sin">с подкрепата на<br><span id="footer_span">Tianaderrah Foundation</span></p>
+               </div>
+               <div class="col-md-2">
+                   <ul>
+                       <li>
+                           <a  href="{{ URL::to('/program')}}">Програма</a>
+                       </li>
+                       <li>
+                           <a  href="{{ URL::to('/terms')}}">Регламент</a>
+                       </li>
+                       <li>
+                           <a href="{{ URL::to('/apply-for-attendance')}}">Форма за участие</a>
+                       </li>
+                       <li>
+                           <a  href="{{ URL::to('/vision')}}">Визия</a>
+                       </li>
+                   </ul>
+               </div>
+               <div class="col-md-2">
+                   <ul>
+                       <li>
+                           <a href="#">Стипендии за бакалавър</a>
+                       </li>
+                       <li>
+                           <a href="#">Стипендии за магистър</a>
+                       </li>
+                       <li>
+                           <a href="#">Стипендии за доктор</a>
+                       </li>
+                       <li>
+                           <a href="{{ URL::to('/authors')}}">Автори</a>
+                       </li>
+                   </ul>
+               </div>
+               <div class="col-md-1">
+                   <ul>
+                       <li>
+                           <a href="#">Изложби</a>
+                       </li>
+                       <li>
+                           <a href="#">Ателиета</a>
+                       </li>
+                       <li>
+                           <a href="#">Лекции</a>
+                       </li>
+                       <li>
+                           <a href="#">Галерия</a>
+                       </li>
+                   </ul>
+               </div>
+               <div class="col-md-2">
+                   <ul>
+                       <li>
+                           <a href="{{ URL::to('/about')}}">За нас</a>
+                       </li>
+                       <li>
+                           <a href="{{ URL::to('/partners')}}">Партньори</a>
+                       </li>
+                       <li>
+                           <a href="{{ URL::to('/contact')}}">Контакти</a>
+                       <li>
+                           <a href="{{ URL::to('/archive')}}">Архив</a>
+                       </li>
+                   </ul>
+               </div>
+               <div class="col-md-2">
+                   <form>
+                       <label for="email" id="Newsletter">Месечен бюлетин</label><br>
+                       <input type="email" name="email" placeholder="e-mail" id="e-mail"><br><br>
+                       <button type="submit" class="btn-submit">Запиши се</button>
+                   </form>
+               </div>
+           </div>
+       </div>
 
-    </div>
+   </div>
 </footer>
