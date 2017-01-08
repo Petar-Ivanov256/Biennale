d0589e5ed316181f3ded64d64450bda635420290
Scipo <ivots@mail.bg>
2017-01-08 22:18:25 +0200

    author.blade.php is almost complete what remains is pictures to pop up on click

--- resources/views/authors.blade.php
+++ resources/views/authors.blade.php
@@ -4,30 +4,44 @@
 @endsection
 
 @section('content')
-
     <div class="container">
         <div class="row">
-            <div class="col-md-12 col-lg-12">
-                <div class="col-md-5 col-lg-5 contestants">
-                    <h2>Автори <span>2017</span></h2>
+            <div class="col-xs-12 col-sm-6 col-md-3 SideDate">
+                <p>27.09-02.10</p>
+            </div>
+        </div>
+    </div>
+    <div  class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-sm-1">
+                <p id="SuzIstoria">Създаваме история</p>
+                <p id="purvoMezdunarodno">Първото международно<br>биенале за стъкло<br>в България</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 authors-move" id="return-authors">
+                <div class="col-md-4 col-lg-4 contestants">
+                    <h2 class="title-Authors">Автори <span class="span-authors-data">2017</span></h2>
                     <hr>
-                    <ul>
-                        <li><a href="#1" data-toggle="collapse">Alex Smith<br><span>Великобритания</span></a></li>
-                        <li><a>Ana Markowska<br><span>Полша</span></a></li>
-                        <li><a>Anton Ivanov<br><span>България</span></a></li>
+                    <ul class="authors-people" id="space-author-li">
+                        <li><a href="#1" data-toggle="collapse">Alex Smith<br><span class="country-authors">Великобритания</span></a></li>
+                        <li><a>Ana Markowska<br><span class="country-authors">Полша</span></a></li>
+                        <li><a>Anton Ivanov<br><span class="country-authors">България</span></a></li>
                     </ul>
                 </div>
                 <div class="col-md-5 col-lg-5">
-                    <h2>Информация и творби</h2>
+                    <h2 class="title-Authors">Информация и творби</h2>
                     <hr>
                     <div id="1" class="collapse">
-                        <h3>Alex Smith</h3>
-                        <ul>
+                        <h3 class="info-name">Alex Smith</h3>
+                        <ul class="info-about-person">
                             <li>Bulgaria</li>
                             <li>male</li>
                             <li>b. Varna</li>
                         </ul>
-                        <p>
+                        <p class="info-autor-about">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel nisi pulvinar, laoreet turpis quis,
                             venenatis turpis. Aenean egestas, nibh sed scelerisque iaculis, risus tortor lobortis arcu, non
                             consectetur mi nibh id ex. Quisque ultricies pellentesque aliquam. Sed pretium nunc at turpis feugiat
@@ -35,22 +49,33 @@
                             per inceptos himenaeos. Fusce eu nisi lacinia, gravida nibh a, tristique magna. Cras sem est, lacinia
                             et laoreet sit amet, semper id erat. Sed dapibus lorem ut pellentesque laoreet. In quis erat nisl.
                             </p>
+                        <br>
                         <div class="authors-pic">
                             <div class="col-md-6">
                                 <div>
                                     {!! HTML::image('img/authors/pic1autr.jpg') !!}
-                                    <br>
-                                    <p>
-                                        Duis aute irure dolor in reprehenderit<br> <span>in volup</span>
-                                    </p>
-                                </div>
+                                    <br><br>
+                                   <div class="info-authors-backgcolor" id="backgcolo-info-authors">
+                                       <p class="info-authors-text-under-pic">
+                                           Duis aute irure dolor in reprehenderit
+                                       </p>
+                                       <p class="info-about-tech">
+                                           in volup
+                                       </p><br><br>
+                                   </div>
+                                </div><br>
                            </div>
                             <div class="col-md-6">
                                 {!! HTML::image('img/authors/pic1autr.jpg') !!}
-                                <br>
-                                <p>
-                                    Duis aute irure dolor in reprehenderit<br> <span>in volup</span>
-                                </p>
+                                <br><br>
+                                <div class="info-authors-backgcolor" id="backgcolo-info-authors">
+                                    <p class="info-authors-text-under-pic">
+                                        Duis aute irure dolor in reprehenderit
+                                    </p>
+                                    <p class="info-about-tech">
+                                        in volup
+                                    </p><br><br>
+                                </div>
                             </div>
                         </div>
 
