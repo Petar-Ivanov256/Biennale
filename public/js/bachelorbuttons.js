208c4b61d362e876440919468b7ebada32abe28b
Scipo <ivots@mail.bg>
2017-01-16 20:12:44 +0200

    Archive has  CSS and some functionality

--- public/js/bachelorbuttons.js
+++ public/js/bachelorbuttons.js
@@ -11,6 +11,21 @@ $(".learn-more").one('click', function(){
     $(this).addClass("setColor");
 });
 
+
+$(document).ready(function() {
+    var $b = $('.buttons-archive');
+    var $p = $('.second-p-paragraph');
+
+    $b.click(function() {
+        var i = $b.index(this);
+        $p.hide().eq(i).show()
+    })
+});
+
+$(".buttons-archive").one('click', function(){
+    $(this).addClass("setColor");
+});
+
 var inputs = document.querySelectorAll( '.inputfile' );
 Array.prototype.forEach.call( inputs, function( input )
 {
--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -21,19 +21,19 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-12 clo-lg-12">
-                <div class="col-md-4 move-left-archive">
-                    <h2>Архив <span>2017</span></h2>
-                    <p>
+            <div class="col-md-12 clo-lg-12 move-left-archive">
+                <div class="col-md-5 col-lg-5">
+                    <h2 class="header-archive">Архив <span>2017</span></h2>
+                    <p class="paragraph-archive-header">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula felis justo, pharetra
                         ullamcorper nibh tristique in. Aenean fringilla dignissim ante eget ornare. Sed eu velit quis
                         velit suscipit consectetur pretium quis ligula.Phasellus tincidunt faucibus lorem sit amet sodales. Duis nec consectetur mauris.
                     </p>
                     <ul class="a-manage-collapse">
-                        <li><a class="" role="button" data-toggle="collapse" href="#" aria-expanded="false" aria-controls="collapseExample">
+                        <li><a class="" role="button" data-toggle="collapse" href="#artists" aria-expanded="false" aria-controls="collapseExample">
                                 <span class="left-archive-span">Артисти</span><span class="color-black-hover"><i class="fa fa-angle-double-right" aria-hidden="true"></i></span>
                             </a></li>
-                        <li><a class="" role="button" data-toggle="collapse" href="#" aria-expanded="false" aria-controls="collapseExample">
+                        <li><a class="" role="button" data-toggle="collapse" href="#event" aria-expanded="false" aria-controls="collapseExample">
                                 <span class="left-archive-span">Събития и локации</span> <span><i class="fa fa-angle-double-right" aria-hidden="true"></i></span>
                             </a></li>
                         <li><a class="" role="button" data-toggle="collapse" href="#" aria-expanded="false" aria-controls="collapseExample">
@@ -44,9 +44,20 @@
                             </a></li>
                     </ul>
                 </div>
-                <div class="col-md-4 move-left-archive">
-                    <button class="learn-more">Победители</button>
-                    <button class="learn-more">Участници</button>
+                <div class="col-md-4 col-lg-4 collapse" id="artists">
+                    <button class="buttons-archive">Победители</button>
+                    <button class="buttons-archive">Участници</button>
+                    <br><br><br>
+                    <div class="second-p-paragraph"><br>
+                        <ul class="authors-people" id="space-author-li">
+                            <li><a href="#1" data-toggle="collapse">Alex Smith<br><span class="country-authors">Великобритания</span></a></li>
+                            <li><a>Ana Markowska<br><span class="country-authors">Полша</span></a></li>
+                            <li><a>Anton Ivanov<br><span class="country-authors">България</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4 collapse" id="event">
+                    <p>Events and Locations<p>
                 </div>
             </div>
          </div>
