a2cf9ee73160e1f272b53d1d1ea08dc99f39e1f8
Scipo <ivots@mail.bg>
2017-02-07 14:20:12 +0200

    minor fix in home

--- resources/views/applicationForm.blade.php
+++ resources/views/applicationForm.blade.php
@@ -4,14 +4,14 @@
 @endsection
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-3 SideDate">
                 <p>27.09-02.10</p>
             </div>
         </div>
     </div>
-    <div  class="container">
+    <div  class="container-fluid">
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-sm-1">
                 <p id="SuzIstoria">Създаваме история</p>
