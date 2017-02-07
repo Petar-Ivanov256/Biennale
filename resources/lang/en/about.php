fc44706d5152437d873c6e4ad3ac99b0cf8fe7be
Scipo <ivots@mail.bg>
2017-02-07 17:35:24 +0200

    some fixes for wide screens

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -2,7 +2,15 @@
 
 return [
     'vision' => 'Our vision',
-    'vision_content' => 'International Biennale of Glass is one-of-a-kind event for the country of Bulgaria, which takes place in late September each two years. It is held in some of the main cities in the country – the capital of Sofia, the marine city of Varna and the town of Beloslav with its glass factory. Our vision is a community where art becomes an integral part of peoples\' lives to create a vivid future for the generations to be. To bring that vision into reality, we inspire glass artists around the world to work, exhibit and share their knowledge, with the only aim to evolve art glass in Europe and especially in its eastern region. We recognize specific values as essential for reaching our vision and mission. As a team of artists ourselves we pledge that our appearance in the field of fine arts is governed by: respect, cooperation, quality, novelty, development of knowledge, inspiration and positive thinking.',
+    'vision_content' => 'International Biennale of Glass is one-of-a-kind event for the country of Bulgaria, which aims  to develop glass art around the world. 
+                         The biennale week takes place in several cities in the months of September-October each two years. The year of the biennale is dedicated 
+                         to events concerning various manifestations of glass as a medium of expression, production and consumption. The programme within the period 
+                         of one year – the year of the biennale, includes workshops for children, lectures, demonstrations, exhibitions, installations, campaigns etc.',
+    'vision_content_second'=>'Our vision is a community where art becomes an integral part of peoples\' lives. To bring that vision into reality, we inspire glass 
+                             artists around the world to work, exhibit and share their knowledge, with the only aim to evolve art glass in Europe and especially in 
+                             its eastern region. We recognize specific values as essential for reaching our vision and mission. As a team of artists ourselves we pledge
+                             that our appearance in the field of fine arts is governed by: respect, cooperation, quality, novelty, development of knowledge, inspiration 
+                             and positive thinking.',
     'art_director' => 'Art Director',
     'team' => 'Team',
     'coordinators' => 'Coordinators',
