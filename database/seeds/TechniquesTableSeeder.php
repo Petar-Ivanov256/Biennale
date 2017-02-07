c09ad81151e1995f8c176e3c725093c700d2c37b
DimitarBakardzhiev <dimitar.bakardzhiev94@gmail.com>
2017-02-07 17:35:48 +0200

    Techniques added.

--- database/seeds/TechniquesTableSeeder.php
+++ database/seeds/TechniquesTableSeeder.php
@@ -12,81 +12,36 @@ class TechniquesTableSeeder extends Seeder
      */
     public function run()
     {
-        $blownGlass = new Technique();
-        $blownGlass->name = 'blown glass';
-        $blownGlass->save();
-        
-        $kilnCastingGlass = new Technique();
-        $kilnCastingGlass->name = 'kiln casting glass';
-        $kilnCastingGlass->save();
-        
-        $pateDeVerre = new Technique();
-        $pateDeVerre->name = 'pate de verre';
-        $pateDeVerre->save();
-        
-        $molded = new Technique();
-        $molded->name = 'molded';
-        $molded->save();
-        
-        $fusedGlass = new Technique();
-        $fusedGlass->name = 'fused glass';
-        $fusedGlass->save();
-        
-        $slumping = new Technique();
-        $slumping->name = 'slumping';
-        $slumping->save();
-        
-        $freeFall = new Technique();
-        $freeFall->name = 'free fall';
-        $freeFall->save();
-        
-        $glassWithInclusions = new Technique();
-        $glassWithInclusions->name = 'glass with inclusions';
-        $glassWithInclusions->save();
-        
-        $floatGlass = new Technique();
-        $floatGlass->name = 'float glass';
-        $floatGlass->save();
-        
-        $stainedGlass = new Technique();
-        $stainedGlass->name = 'stained glass';
-        $stainedGlass->save();
-        
-        $paintedGlass = new Technique();
-        $paintedGlass->name = 'painted glass';
-        $paintedGlass->save();
-        
-        $printOnGlass = new Technique();
-        $printOnGlass->name = 'print on glass';
-        $printOnGlass->save();
-        
-        $readyMade = new Technique();
-        $readyMade->name = 'ready - made';
-        $readyMade->save();
-        
-        $sandBlastedGlass = new Technique();
-        $sandBlastedGlass->name = 'sand blasted glass';
-        $sandBlastedGlass->save();
-        
-        $engraved = new Technique();
-        $engraved->name = 'engraved';
-        $engraved->save();
-        
-        $coldWorked = new Technique();
-        $coldWorked->name = 'cold-worked';
-        $coldWorked->save();
-        
-        $laminatedGlass = new Technique();
-        $laminatedGlass->name = 'laminated glass';
-        $laminatedGlass->save();
-        
-        $mixedTechnique = new Technique();
-        $mixedTechnique->name = 'mixed technique';
-        $mixedTechnique->save();
+        $allTechniques = [
+            'blown glass',
+            'kiln casting glass',
+            'pate de verre',
+            'molded',
+            'fused glass',
+            'slumping',
+            'free fall',
+            'glass with inclusions',
+            'float glass',
+            'stained glass',
+            'painted glass',
+            'print on glass',
+            'ready - made',
+            'sand blasted glass',
+            'engraved',
+            'cold-worked',
+            'laminated glass',
+            'mixed technique',
+            'lampworking',
+            'etching',
+            'layered glass',
+            'other'
+        ];
 
-        $lampworking = new Technique();
-        $lampworking->name = 'lampworking';
-        $lampworking->save();
+        foreach ($allTechniques as $technique) {
+            DB::table('techniques')->insert([
+                'name' => $technique
+            ]);
+        }
 
         // todo: other
     }
