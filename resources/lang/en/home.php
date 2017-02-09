712b79614ebb9b01002d278c76c856eb2ae1bd15
Scipo <ivots@mail.bg>
2017-02-09 21:40:20 +0200

    little change in translation home.php - en

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -2,6 +2,6 @@
 return [
     'text-p'=>'International biennale of glass is the first of its kind event in Bulgaria. 
                It will be held at the end of September every two years.
-               The biennale will take place in three cities - capital city of  Sofia, 
+               The biennale will take place in three cities - city of  Sofia, 
                city of Varna and town of Beloslav, known  for its glass factory.',
 ];
