f599a4a8b2932237b79a05700b3fba0450a2c2d6
Petar Ivanov <petar.ivanov256@gmail.com>
2017-02-11 16:34:04 +0200

    Admin home button postion updated

--- resources/views/admin/admin_home.blade.php
+++ resources/views/admin/admin_home.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
     <div class="container">
-        <div class="col-md-9">
+        <div  class="col-md-12" style="margin-left: 250px">
             <a href="{{ URL::to('/admin/showEvents')}}" class="btn btn-primary">Виж събития</a>
             <a href="{{ URL::to('/admin/authors')}}" class="btn btn-primary">Виж авторите</a>
             <a href="{{ URL::to('/admin/works')}}" class="btn btn-primary">Виж творбите</a>
