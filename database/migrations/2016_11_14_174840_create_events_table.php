f01511d40f129f1c74608bbd7cee2e24e019218e
Petar Ivanov <petar.ivanov256@gmail.com>
2017-02-21 20:33:16 +0200

    15 photo fields in events added

--- database/migrations/2016_11_14_174840_create_events_table.php
+++ database/migrations/2016_11_14_174840_create_events_table.php
@@ -28,13 +28,42 @@ class CreateEventsTable extends Migration {
             $table->string("title_en")->nullable();
             $table->string("description", 2000);
             $table->string("description_en", 2000)->nullable();
-            $table->binary("photo")->nullable();
+            $table->binary("photo1")->nullable();
+            $table->binary("photo2")->nullable();
+            $table->binary("photo3")->nullable();
+            $table->binary("photo4")->nullable();
+            $table->binary("photo5")->nullable();
+            $table->binary("photo6")->nullable();
+            $table->binary("photo7")->nullable();
+            $table->binary("photo8")->nullable();
+            $table->binary("photo9")->nullable();
+            $table->binary("photo10")->nullable();
+            $table->binary("photo11")->nullable();
+            $table->binary("photo12")->nullable();
+            $table->binary("photo13")->nullable();
+            $table->binary("photo14")->nullable();
+            $table->binary("photo15")->nullable();
             $table->string('latitude')->nullable();
             $table->string('longitude')->nullable();
             $table->timestamps();
         });
 
-        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo` `photo` MEDIUMBLOB NULL AFTER `description`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo1` `photo1` MEDIUMBLOB NULL AFTER `description`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo2` `photo2` MEDIUMBLOB NULL AFTER `photo1`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo3` `photo3` MEDIUMBLOB NULL AFTER `photo2`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo4` `photo4` MEDIUMBLOB NULL AFTER `photo3`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo5` `photo5` MEDIUMBLOB NULL AFTER `photo4`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo6` `photo6` MEDIUMBLOB NULL AFTER `photo5`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo7` `photo7` MEDIUMBLOB NULL AFTER `photo6`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo8` `photo8` MEDIUMBLOB NULL AFTER `photo7`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo9` `photo9` MEDIUMBLOB NULL AFTER `photo8`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo10` `photo10` MEDIUMBLOB NULL AFTER `photo9`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo11` `photo11` MEDIUMBLOB NULL AFTER `photo10`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo12` `photo12` MEDIUMBLOB NULL AFTER `photo11`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo13` `photo13` MEDIUMBLOB NULL AFTER `photo12`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo14` `photo14` MEDIUMBLOB NULL AFTER `photo13`");
+        DB::statement("ALTER TABLE `events` CHANGE COLUMN `photo15` `photo15` MEDIUMBLOB NULL AFTER `photo14`");
+
     }
 
     /**
