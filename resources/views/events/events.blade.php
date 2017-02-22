54a71255be779f036a3de56575d44291904e659e
Petar Ivanov <petar.ivanov256@gmail.com>
2017-02-22 22:06:03 +0200

    Gallery bug fix

--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -99,8 +99,12 @@
     <div class="row">
         <div class="col-md-5 programadetails" id="details">
             <h2 id="event-category"></h2>
-            <img src="" id="details-img">
-            <div class="gallery-container" id="gallery-container"></div>
+            {{--<img src="" id="details-img">--}}
+            <div class="gallery-container" id="gallery-container">
+                <a class="gallery" id="img-ref" href="">
+                    <img src="" id="details-img">
+                </a>
+            </div>
             <div class="ptagleftprogramdetails">
                 <span class="oneprogdet">{{ trans('events.date') }}</span>
                 <span class="twoprogdet" id="details-date"></span>
@@ -151,50 +155,69 @@ function getDetails(id, locale) {
             $('#details-artist').text(event.event.participants_en);
             $('#details-entrance').text(event.event.entrance_en);
             $('#details-img').attr('src', event.event.photo1);
+            $('#img-ref').attr('href', event.event.photo1);
             if(event.event.photo2 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo2" href=""><img class="gal-img" id="img2" src=""/></a>');
-                $('#photo2').attr('href', event.event.photo2);
-                $('#img2').attr('src', event.event.photo2);
+                if (!$('#img2').length){
+                    $('#gallery-container').append('<a class="gallery" id="photo2" href=""><img class="gal-img" id="img2" src=""/></a>');
+                    $('#photo2').attr('href', event.event.photo2);
+                    $('#img2').attr('src', event.event.photo2);
+                }
             }
             if(event.event.photo3 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo3" href=""><img class="gal-img" id="img3" src=""/></a>');
-                $('#photo3').attr('href', event.event.photo3);
-                $('#img3').attr('src', event.event.photo3);
+                if (!$('#img3').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo3" href=""><img class="gal-img" id="img3" src=""/></a>');
+                    $('#photo3').attr('href', event.event.photo3);
+                    $('#img3').attr('src', event.event.photo3);
+                }
             }
             if(event.event.photo4 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo4" href=""><img class="gal-img" id="img4" src=""/></a>');
-                $('#photo4').attr('href', event.event.photo4);
-                $('#img4').attr('src', event.event.photo4);
+                if (!$('#img4').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo4" href=""><img class="gal-img" id="img4" src=""/></a>');
+                    $('#photo4').attr('href', event.event.photo4);
+                    $('#img4').attr('src', event.event.photo4);
+                }
             }
             if(event.event.photo5 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo5" href=""><img class="gal-img" id="img5" src=""/></a>');
-                $('#photo5').attr('href', event.event.photo5);
-                $('#img5').attr('src', event.event.photo5);
+                if (!$('#img5').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo5" href=""><img class="gal-img" id="img5" src=""/></a>');
+                    $('#photo5').attr('href', event.event.photo5);
+                    $('#img5').attr('src', event.event.photo5);
+                }
             }
             if(event.event.photo6 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo6" href=""><img class="gal-img" id="img6" src=""/></a>');
-                $('#photo6').attr('href', event.event.photo6);
-                $('#img6').attr('src', event.event.photo6);
+                if (!$('#img6').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo6" href=""><img class="gal-img" id="img6" src=""/></a>');
+                    $('#photo6').attr('href', event.event.photo6);
+                    $('#img6').attr('src', event.event.photo6);
+                }
             }
             if(event.event.photo7 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo7" href=""><img class="gal-img" id="img7" src=""/></a>');
-                $('#photo7').attr('href', event.event.photo7);
-                $('#img7').attr('src', event.event.photo7);
+                if (!$('#img7').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo7" href=""><img class="gal-img" id="img7" src=""/></a>');
+                    $('#photo7').attr('href', event.event.photo7);
+                    $('#img7').attr('src', event.event.photo7);
+                }
             }
             if(event.event.photo8 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo8" href=""><img class="gal-img" id="img8" src=""/></a>');
-                $('#photo8').attr('href', event.event.photo8);
-                $('#img8').attr('src', event.event.photo8);
+                if (!$('#img8').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo8" href=""><img class="gal-img" id="img8" src=""/></a>');
+                    $('#photo8').attr('href', event.event.photo8);
+                    $('#img8').attr('src', event.event.photo8);
+                }
             }
             if(event.event.photo9 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo9" href=""><img class="gal-img" id="img9" src=""/></a>');
-                $('#photo9').attr('href', event.event.photo9);
-                $('#img9').attr('src', event.event.photo9);
+                if (!$('#img9').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo9" href=""><img class="gal-img" id="img9" src=""/></a>');
+                    $('#photo9').attr('href', event.event.photo9);
+                    $('#img9').attr('src', event.event.photo9);
+                }
             }
             if(event.event.photo10 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo10" href=""><img class="gal-img" id="img10" src=""/></a>');
-                $('#photo10').attr('href', event.event.photo10);
-                $('#img10').attr('src', event.event.photo10);
+                if (!$('#img10').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo10" href=""><img class="gal-img" id="img10" src=""/></a>');
+                    $('#photo10').attr('href', event.event.photo10);
+                    $('#img10').attr('src', event.event.photo10);
+                }
             }
             $('#event-description').html(event.event.description_en);
         });
@@ -213,50 +236,69 @@ function getDetails(id, locale) {
             $('#details-artist').text(event.event.participants);
             $('#details-entrance').text(event.event.entrance);
             $('#details-img').attr('src', event.event.photo1);
+            $('#img-ref').attr('href', event.event.photo1);
             if(event.event.photo2 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo2" href=""><img class="gal-img" id="img2" src=""/></a>');
-                $('#photo2').attr('href', event.event.photo2);
-                $('#img2').attr('src', event.event.photo2);
+                if (!$('#img2').length){
+                    $('#gallery-container').append('<a class="gallery" id="photo2" href=""><img class="gal-img" id="img2" src=""/></a>');
+                    $('#photo2').attr('href', event.event.photo2);
+                    $('#img2').attr('src', event.event.photo2);
+                }
             }
             if(event.event.photo3 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo3" href=""><img class="gal-img" id="img3" src=""/></a>');
-                $('#photo3').attr('href', event.event.photo3);
-                $('#img3').attr('src', event.event.photo3);
+                if (!$('#img3').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo3" href=""><img class="gal-img" id="img3" src=""/></a>');
+                    $('#photo3').attr('href', event.event.photo3);
+                    $('#img3').attr('src', event.event.photo3);
+                }
             }
             if(event.event.photo4 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo4" href=""><img class="gal-img" id="img4" src=""/></a>');
-                $('#photo4').attr('href', event.event.photo4);
-                $('#img4').attr('src', event.event.photo4);
+                if (!$('#img4').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo4" href=""><img class="gal-img" id="img4" src=""/></a>');
+                    $('#photo4').attr('href', event.event.photo4);
+                    $('#img4').attr('src', event.event.photo4);
+                }
             }
             if(event.event.photo5 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo5" href=""><img class="gal-img" id="img5" src=""/></a>');
-                $('#photo5').attr('href', event.event.photo5);
-                $('#img5').attr('src', event.event.photo5);
+                if (!$('#img5').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo5" href=""><img class="gal-img" id="img5" src=""/></a>');
+                    $('#photo5').attr('href', event.event.photo5);
+                    $('#img5').attr('src', event.event.photo5);
+                }
             }
             if(event.event.photo6 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo6" href=""><img class="gal-img" id="img6" src=""/></a>');
-                $('#photo6').attr('href', event.event.photo6);
-                $('#img6').attr('src', event.event.photo6);
+                if (!$('#img6').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo6" href=""><img class="gal-img" id="img6" src=""/></a>');
+                    $('#photo6').attr('href', event.event.photo6);
+                    $('#img6').attr('src', event.event.photo6);
+                }
             }
             if(event.event.photo7 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo7" href=""><img class="gal-img" id="img7" src=""/></a>');
-                $('#photo7').attr('href', event.event.photo7);
-                $('#img7').attr('src', event.event.photo7);
+                if (!$('#img7').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo7" href=""><img class="gal-img" id="img7" src=""/></a>');
+                    $('#photo7').attr('href', event.event.photo7);
+                    $('#img7').attr('src', event.event.photo7);
+                }
             }
             if(event.event.photo8 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo8" href=""><img class="gal-img" id="img8" src=""/></a>');
-                $('#photo8').attr('href', event.event.photo8);
-                $('#img8').attr('src', event.event.photo8);
+                if (!$('#img8').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo8" href=""><img class="gal-img" id="img8" src=""/></a>');
+                    $('#photo8').attr('href', event.event.photo8);
+                    $('#img8').attr('src', event.event.photo8);
+                }
             }
             if(event.event.photo9 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo9" href=""><img class="gal-img" id="img9" src=""/></a>');
-                $('#photo9').attr('href', event.event.photo9);
-                $('#img9').attr('src', event.event.photo9);
+                if (!$('#img9').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo9" href=""><img class="gal-img" id="img9" src=""/></a>');
+                    $('#photo9').attr('href', event.event.photo9);
+                    $('#img9').attr('src', event.event.photo9);
+                }
             }
             if(event.event.photo10 != null){
-                $('#gallery-container').append('<a class="gallery" id="photo10" href=""><img class="gal-img" id="img10" src=""/></a>');
-                $('#photo10').attr('href', event.event.photo10);
-                $('#img10').attr('src', event.event.photo10);
+                if (!$('#img10').length) {
+                    $('#gallery-container').append('<a class="gallery" id="photo10" href=""><img class="gal-img" id="img10" src=""/></a>');
+                    $('#photo10').attr('href', event.event.photo10);
+                    $('#img10').attr('src', event.event.photo10);
+                }
             }
             $('#event-description').html(event.event.description);
         });
