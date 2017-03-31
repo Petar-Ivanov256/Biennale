17f90fda2e8768961fdbda7556c371e5e720f8d6
Scipo <ivots@mail.bg>
2017-03-31 21:58:43 +0300

    the old new

--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -103,7 +103,7 @@
         <div class="col-md-5 programadetails" id="details">
             <h2 id="event-category"></h2>
             {{--<img src="" id="details-img">--}}
-            <div class="gallery-container" id="gallery-container">
+            <div class="gallery-container event_program_details" id="gallery-container">
                 <a class="gallery" id="img-ref" href="">
                     <img src="" id="details-img">
                 </a>
@@ -349,7 +349,21 @@ function getDetails(id, locale) {
             },
             gallery: {
                 enabled:true
+            },
+            zoom: {
+                enabled: true, // By default it's false, so don't forget to enable it
+                duration: 300, // duration of the effect, in milliseconds
+                easing: 'ease-in-out', // CSS transition easing function
+                // The "opener" function should return the element from which popup will be zoomed in
+                // and to which popup will be scaled down
+                // By defailt it looks for an image tag:
+                opener: function(openerElement) {
+                    // openerElement is the element on which popup was initialized, in this case its <a> tag
+                    // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                    return openerElement.is('img') ? openerElement : openerElement.find('img');
+                }
             }
+
         });
     });
 </script>
