c964bb4ebe40ec9108b6d56e071f06bcd8470440
Scipo <ivots@mail.bg>
2017-06-30 13:43:51 +0300

    The logos are improved

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -31,10 +31,10 @@
                 </div><br>
                 <h4 class="partnersName2">{{ trans('partners.main_partners') }}</h4><br>
                 <div class="ahover size_img_partners_f">
-                    <a href="http://www.nbu.bg/en" target="_blank">{!! HTML::image('img/partnersimg/nbu.jpg','alt',array('width'=>110,'height'=>85)) !!}</a>&nbsp;&nbsp;
-                    <a href="http://afbulgaria.org/" target="_blank">{!! HTML::image('img/partnersimg/AFBBG2.png','alt',array('width'=>80,'height'=>80)) !!}</a>&nbsp;&nbsp;
+                    <a href="http://www.nbu.bg/en" target="_blank">{!! HTML::image('img/partnersimg/nbu.png','alt',array('width'=>110,'height'=>85)) !!}</a>&nbsp;&nbsp;
+                    <a href="http://afbulgaria.org/" target="_blank">{!! HTML::image('img/partnersimg/AFBBG3.png','alt',array('width'=>80,'height'=>80)) !!}</a>&nbsp;&nbsp;
                     <a href="http://momkasglass.com/bg/" target="_blank">{!! HTML::image('img/partnersimg/Momka.png','alt',array('width'=>70,'height'=>70)) !!}</a>&nbsp;&nbsp;
-                    <a href="http://inhomglass.com/" target="_blank" id="resize">{!! HTML::image('img/partnersimg/inhom1.jpg','alt',array('width'=>170,'height'=>52)) !!}</a>&nbsp;&nbsp;
+                    <a href="http://inhomglass.com/" target="_blank" id="resize">{!! HTML::image('img/partnersimg/inhom1.png','alt',array('width'=>170,'height'=>52)) !!}</a>&nbsp;&nbsp;
                     <a href="http://www.kristian-neiko.com/" target="_blank">{!! HTML::image('img/partnersimg/KrN.png','alt',array('width'=>152,'height'=>30)) !!}</a><br><br>&nbsp;&nbsp;
                     <a href="http://www.eurobulsoft.com/" target="_blank">{!! HTML::image('img/partnersimg/eurobulsoft_logo.png','alt',array('width'=>160,'height'=>60)) !!}</a>&nbsp&nbsp;
                     <a href="http://www.greenpeace.org/bulgaria/bg/" target="_blank">{!! HTML::image('img/partnersimg/Greenpeace.svg.png','alt',array('width'=>200,'height'=>30)) !!}</a>&nbsp;&nbsp;<br><br>
@@ -46,14 +46,14 @@
                     <a href="http://www.uni-vt.bg/bul/" target="_blank">{!! HTML::image('img/partnersimg/vtu.png','alt',array('width'=>100,'height'=>120)) !!}</a>&nbsp;
                     <a href="http://ngpisvetiluka.com/" target="_blank">{!! HTML::image('img/partnersimg/rumi1.png','alt',array('width'=>120,'height'=>130)) !!}</a>&nbsp;
                     <a href="http://www.sofia.bg/" target="_blank">{!! HTML::image('img/partnersimg/Sofia.svg.png','alt',array('width'=>100,'height'=>120)) !!}</a>&nbsp;&nbsp;
-                    <a href="http://www.sofiahistorymuseum.bg/" target="_blank">{!! HTML::image('img/partnersimg/GIM_Sofia_Logo.jpg','alt',array('width'=>100,'height'=>100)) !!}</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="http://www.sofiahistorymuseum.bg/" target="_blank">{!! HTML::image('img/partnersimg/GIM_Sofia_Logo.png','alt',array('width'=>100,'height'=>100)) !!}</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <a href="http://www.varna.bg/" target="_blank">{!! HTML::image('img/partnersimg/varna.png','alt',array('width'=>90,'height'=>120)) !!}</a>&nbsp;&nbsp;
-                    <a href="https://culturalcosmos.com/" target="_blank">{!! HTML::image('img/partnersimg/CinemaCosmos_logo_bg.jpg','alt',array('width'=>100,'height'=>60)) !!}</a>&nbsp;&nbsp;
-                    <a href="http://www.uniart.bg/" target="_blank">{!! HTML::image('img/partnersimg/uniart1.jpg','alt',array('width'=>100,'height'=>120)) !!}</a>&nbsp;
-                    <a href="http://www.chervenatatochka.bg/en/" target="_blank">{!! HTML::image('img/partnersimg/chervenatochka.jpg','alt',array('width'=>115,'height'=>40)) !!}</a>
-                    <a href="http://sofia.czechcentres.cz/" target="_blank">{!! HTML::image('img/partnersimg/czcenter.jpg','alt',array('width'=>130,'height'=>40)) !!}</a>&nbsp;&nbsp;
+                    <a href="https://culturalcosmos.com/" target="_blank">{!! HTML::image('img/partnersimg/CinemaCosmos_logo_bg.png','alt',array('width'=>100,'height'=>60)) !!}</a>&nbsp;&nbsp;
+                    <a href="http://www.uniart.bg/" target="_blank">{!! HTML::image('img/partnersimg/uniart3.png','alt',array('width'=>100,'height'=>120)) !!}</a>&nbsp;
+                    <a href="http://www.chervenatatochka.bg/en/" target="_blank">{!! HTML::image('img/partnersimg/chervenatochka.png','alt',array('width'=>115,'height'=>40)) !!}</a>
+                    <a href="http://sofia.czechcentres.cz/" target="_blank">{!! HTML::image('img/partnersimg/czcenter.png','alt',array('width'=>130,'height'=>40)) !!}</a>&nbsp;&nbsp;
                     <a href="http://rgfoundation.blogspot.bg/" target="_blank">{!! HTML::image('img/partnersimg/logoR.png','alt',array('width'=>170,'height'=>45)) !!}</a>&nbsp;&nbsp;
-                    <a href="http://galeriemeridian.cz/" target="_blank" class="gen_partner">Galerie MERIDIAN Prague</a>&nbsp;&nbsp;
+                    <a href="http://galeriemeridian.cz/" target="_blank" class="gen_partner">{!! HTML::image('img/partnersimg/gmer.png','alt',array('width'=>145,'height'=>45)) !!}</a>&nbsp;&nbsp;
                 </div>
                 <br>
                 <h4 class="partnersName2">{{ trans('partners.media_partners') }}</h4><br>
